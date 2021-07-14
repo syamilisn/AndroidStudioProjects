0fb1f25c5ea97b31386d880441edb05209e1a471
syamilisn <59735277+syamilisn@users.noreply.github.com>
2021-07-14 15:46:39 +0530

    updated dicitonary app

--- madlab/Dictionary/app/src/main/java/com/killjoy/dictionary/SearchActivity.java
+++ madlab/Dictionary/app/src/main/java/com/killjoy/dictionary/SearchActivity.java
@@ -3,12 +3,19 @@ package com.killjoy.dictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
+    EditText edit1;
+    Button bn1;
+    String searchWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        edit1=(EditText)findViewById(R.id.ed1);
+        bn1=(Button)findViewById(R.id.b1);
     }
 }
--- madlab/Dictionary/app/src/main/java/com/killjoy/dictionary/WelcomeActivity.java
+++ madlab/Dictionary/app/src/main/java/com/killjoy/dictionary/WelcomeActivity.java
@@ -2,13 +2,49 @@ package com.killjoy.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class WelcomeActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText edit1;
+    Button bn1,bn2;
+    String regularExpression = "^[A-Za-z]{3,20}$";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        edit1=(EditText)findViewById(R.id.ed1);
+        bn1=(Button)findViewById(R.id.b1);
+        bn2=(Button)findViewById(R.id.b2);
+        bn1.setOnClickListener(this);
+        bn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String word = edit1.getText().toString();
+        if(validWord(word)) {
+            Bundle bundle = new Bundle();
+            bundle.putString("word", word);
+            Intent it = new Intent(this, SearchActivity.class);
+            it.putExtra("data", bundle);
+            startActivity(it);
+        }
+        else
+            Toast.makeText(getBaseContext(), "Invalid Word", Toast.LENGTH_LONG).show();
+    }
+    public boolean validWord(String password)
+    {
+        Pattern pattern= Pattern.compile(regularExpression);
+        Matcher matcher=pattern.matcher(password);
+        return matcher.matches();
     }
 }
